0e86fb9d479973f9ac63928c54be9adf90044fcb
Spunch <137802895+Spunchkin@users.noreply.github.com>
2023-11-17 16:05:28 +0400

    Update cheezit.py

--- src/cheezit.py
+++ src/cheezit.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-#!/usr/bin/env python3
-#!/usr/bin/env python3
-#!/usr/bin/env python3
 import os
 import shutil
 import sys
@@ -14,7 +10,10 @@ if argc > 1:
         print("Help: -s type directory : Sort(-s type(1.filetype, 2.name))")
     elif sys.argv[1] == "-s":
         if argc > 2:
-            directory = os.getcwd()
+            if argc == 4:
+                directory = sys.argv[3]
+            else:
+                directory = os.getcwd()
             if sys.argv[2] in ("1", "filetype"):
                 extensions = search.get_unique_file_extensions(directory)
                 for item in extensions:
@@ -31,6 +30,10 @@ if argc > 1:
                                     print("file either does not exist, or has already been moved")
                             else:
                                 pass
+                with open("logs.clog", "w") as log:
+                    log.write(f"Sorted files in {directory}, sorted by type")
+                    log.close()
+
             elif sys.argv[2] in ("2", "name"):
                 for filename in os.listdir(directory):
                     if os.path.isfile(os.path.join(directory, filename)):
@@ -43,5 +46,15 @@ if argc > 1:
                         source_path = os.path.join(directory, filename)
                         destination_path = os.path.join(folder_path, filename)
                         shutil.move(source_path, destination_path)
+                with open("logs.clog", "w") as log:
+                    log.write(f"Sorted files in {directory}, sorted by name")
+
+    elif sys.argv[1] == "-l":
+        with open("logs.clog", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                print(line, end="")
+
+            
 else:
     print("No arguments specified")
